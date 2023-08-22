46f85d9ad45aebb49ba97977e024e43af2dec74c
Ryan Schlimme <98181475+ryanschlimme@users.noreply.github.com>
2023-08-22 09:19:06 -0500

    Update IdleTest.py

--- IdleTest.py
+++ IdleTest.py
@@ -1,8 +1,8 @@
-# File: IdleTest.py
-# Student: Ryan Schlimme
-# UT EID: rjs4499
+# IdleTest.py
+# Ryan Schlimme
+# rjs4499
 #
-# Date: 21 August 2023
-# Description of Program: Testing VSCode IDE for use in CS 303E/313E courses at UT Austin.
+# 21 August 2023
+# Testing VSCode IDE for use in CS 303E/313E courses at UT Austin.
 
 print("Hey, I'm using IDLE!")   # even though I am using VSCode
