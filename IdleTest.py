4df76e79bfdc49800e814aa77f4f23084d5d073e
Ryan Schlimme <98181475+ryanschlimme@users.noreply.github.com>
2023-08-24 08:11:52 -0500

    Added Initials.py
    Homework 1 ONLY ALLOWED PRINT STATEMENTS

--- IdleTest.py
+++ IdleTest.py
@@ -1,6 +1,7 @@
 # IdleTest.py
 # Ryan Schlimme
 # rjs4499
+# CS 303E
 #
 # 21 August 2023
 # Testing VSCode IDE for use in CS 303E/313E courses at UT Austin.
---
+++ Initials.py
@@ -0,0 +1,23 @@
+# Initials.py
+# Ryan Schlimme
+# rjs4499
+# CS 303E
+#
+# 23 August 2023
+# This program prints my initials (RJS) as 12 character wide, 10 character tall "supercharacters". Each line segment is 2 characters thick.
+# Each "supercharacter" is made up of capitals letters corresponding to the letter it creates followed by a period made up of a 2x2 square of periods. 
+# There are also two empty columns on the right and left of each character and period, including the final period. There are also 
+# empty lines at the top and bottom.
+
+print()
+print("  RRRRRRRRRRRR      JJJJJJJJJJJJ      SSSSSSSSSSSS      ")
+print("  RRRRRRRRRRRR      JJJJJJJJJJJJ      SSSSSSSSSSSS      ")
+print("  RR       RR            JJ           SS                ")
+print("  RR      RR             JJ           SS                ")
+print("  RR     RR              JJ           SSSSSSSSSSSS      ")
+print("  RRRRRRRRR              JJ           SSSSSSSSSSSS      ")
+print("  RRRRRRRRR              JJ                     SS      ")
+print("  RR      RR             JJ                     SS      ")
+print("  RR       RR   ..  JJJJJJJ       ..  SSSSSSSSSSSS  ..  ")
+print("  RR        RR  ..  JJJJJJJ       ..  SSSSSSSSSSSS  ..  ")
+print()
